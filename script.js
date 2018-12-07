abcfe2926803b6d8167f8c7bf488ab57ef7c5a68
madihaparacha <madihaparacha96@gmail.com>
2018-12-07 11:45:25 +0500

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.querySelector('.sidenav').classList.remove('slideOutRight');
+    document.querySelector('.sidenav').classList.add('slideInRight');
 }
 
 function closeNav() {
+    document.querySelector('.sidenav').classList.remove('slideInRight');
+    document.querySelector('.sidenav').classList.add('slideOutRight');
     document.getElementById("mySidenav").style.width = "0";
 }
